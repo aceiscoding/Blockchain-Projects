acdd73ccb8e01f2b521026dbb30109e6068d5901
Ace Apolonio <acespencer@yonsei.ac.kr>
2022-07-12 21:48:47 +0900

    completed header design

--- uniswap-clone/client/components/Header.js
+++ uniswap-clone/client/components/Header.js
@@ -7,7 +7,21 @@ import ethLogo from '../assets/eth.png'
 import uniswapLogo from '../assets/uniswap.png'
 
 
-const style = {}
+const style = {
+    wrapper:  `p-4 w-screen flex justify-between items-center`,
+    headerLogo: ` flex w-1/4 items-center justify-start`,
+    nav: `flex-1 flex justify-center items-center`,
+    navItemsContainer: `flex bg-[#191B1F] rounded-3xl`,
+    navItem: `px-4 py-2 m-1 flex items-center text-lg  font-semibold text-[0.9rem] cursor-pointer rounded-3xl `,
+    activeNavItem:  `bg-[#20242A]` , 
+    buttonsContainer: `flex w-1/4 justify-end items-center`,
+    button: `flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold  cursor-pointer`,
+    buttonPadding: `p-2`,
+    buttonTextContainer: `h-8 flex items-center`,
+    buttonIconContainer: `flex items-center justify-center w-8 h-8`,
+    buttonAccent: `bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl flex items justify-center text-[#4F90EA]`,
+
+}
 
 const Header = ( ) => {
     const [selectedNav, setSelectedNav] = useState('swap')
@@ -68,22 +82,22 @@ const Header = ( ) => {
 
                         <div
                         onClick={() => connectWallet()}
-                        className={`${style.button} ${style.buttonPadding}`}>
+                        className={`${style.button} ${style.buttonPadding}`}
+                        >
 
                             <div className={`${style.buttonAccent} ${style.buttonPadding}`}>
-                                <div className={`${style.buttonIconContainer} mx-2`}></div>
-                                    <HiOutlineDotsVertical />
+                                Connect Wallet
                             </div>
 
-                            <div>jkhg</div>
-
-
-
                         </div>
-                            
-                    </div>
-           
-        </div>
+                        
+                            <div className={`${style.buttonAccent} ${style.buttonPadding}`}>
+                                <div className={`${style.buttonIconContainer} mx-2`}>
+                                    <HiOutlineDotsVertical />
+                                 </div>
+                             </div>                           
+                    </div>          
+             </div>
     )
 
 
