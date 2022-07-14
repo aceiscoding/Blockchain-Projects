77f3193b1b056856c75c10f4b0a56158668370d8
Ace Apolonio <acespencer@yonsei.ac.kr>
2022-07-15 00:04:51 +0900

    finished smart contract

--- uniswap-clone/smart_contract/hardhat.config.js
+++ uniswap-clone/smart_contract/hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/W2kVWhHl-Ofek-fCMvTLDx8CIoudWCXi',
+      accounts:  [
+        'f61446c9e7eeb3376d35749aa5f5494de5e10da9181c4dfa8698c05a914900cf',
+      ],
+    },
+  },
 };
